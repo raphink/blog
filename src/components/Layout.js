f4cb76a3f55db4c2cb43cf48f74b48fc484ef127
Raphaël Pinson <raphael@isovalent.com>
2026-06-24 19:42:30 +0200

    Setup GA
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,8 @@ export default class Body extends React.Component {
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt') || _.get(this.props, 'pageContext.site.siteMetadata.description')}/>
                     <meta property="og:title" content={_.get(this.props, 'pageContext.frontmatter.title') || _.get(this.props, 'pageContext.site.siteMetadata.title')}/>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-F1DK6MCNH6"></script>
+                    <script>{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-F1DK6MCNH6');`}</script>
                     <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i&display=swap" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
                     {(_.get(this.props, 'pageContext.frontmatter.template') === 'post') && 

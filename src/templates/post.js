6e975f8af050e0def627d8d5713beb5ab5485ba1
Raphaël Pinson <raphael@isovalent.com>
2026-06-24 19:04:30 +0200

    Add giscus support
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>

--- src/templates/post.js
+++ src/templates/post.js
@@ -72,6 +72,23 @@ export default class Post extends React.Component {
                       Originally published on <a href={_.get(this.props, 'pageContext.frontmatter.blogspot_url')} target="_blank" rel="noopener noreferrer">Blogspot</a>.
                     </footer>
                     }
+                    <div className="giscus-comments">
+                      <script src="https://giscus.app/client.js"
+                        data-repo="raphink/blog"
+                        data-repo-id="MDEwOlJlcG9zaXRvcnkyNjU1MjAwMTQ="
+                        data-category="Announcements"
+                        data-category-id="DIC_kwDOD9ODjs4C_z6L"
+                        data-mapping="og:title"
+                        data-strict="1"
+                        data-reactions-enabled="1"
+                        data-emit-metadata="0"
+                        data-input-position="bottom"
+                        data-theme="preferred_color_scheme"
+                        data-lang={lang === 'fr' ? 'fr' : 'en'}
+                        crossOrigin="anonymous"
+                        async>
+                      </script>
+                    </div>
                   </article>
                 </main>
                 <Footer {...this.props} site={this.props.pageContext.site} page={this.props.pageContext} image={_.get(this.props, 'pageContext.frontmatter.content_img_path')} />
